8875b524fe24f7cf43e8df077ba8390e22497c6e
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-10-05 18:40:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -78,23 +78,72 @@ def hide_streamlit_elements():
 
 def main():
     st.title("Question & Answer Retrieval from PDFs")
+    st.write("This tool was developed by Chancee Vincent to assist in information retrieval from the following documents:")
+
+    with st.beta_expander("DoD"):
+        st.write("""
+        - [DoD RDT&E Budget Justification](https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2024/budget_justification/pdfs/03_RDT_and_E/OSD_PB2024.pdf)
+        - [DoD Procurement Budget Justification](https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2024/budget_justification/pdfs/02_Procurement/PB_2024_PDW_VOL_1.pdf)
+        - [DoD Operation and Maintenance Justification](https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2024/budget_justification/pdfs/01_Operation_and_Maintenance/O_M_VOL_1_PART_1/OM_Volume1_Part1.pdf)
+        """)
+    with st.beta_expander("Air & Space Force"):
+        st.write("""
+        - [Air Force Aircraft Procurement Vol I](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Aircraft%20Procurement%20Vol%20I.pdf)
+        - [Air Force Aircraft Procurement Vol II Mods](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Aircraft%20Procurement%20Vol%20II%20Mods.pdf)
+        - [Air Force Ammunition Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Ammunition%20Procurement.pdf)
+        - [Air Force Missile Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Missile%20Procurement.pdf)
+        - [Air Force Other Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Other%20Procurement.pdf)
+        - [Space Force Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Space%20Force%20Procurement.pdf)
+        - [Air Force RDT&E Vol I](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20I.pdf)
+        - [Air Force RDT&E Vol II](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20II.pdf)
+        - [Air Force RDT&E Vol IIIa](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20IIIa.pdf)
+        - [Air Force RDT&E Vol IIIb](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20IIIb.pdf)
+        - [Space Force RDT&E](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Space%20Force%20Research%20and%20Development%20Test%20and%20Evaluation.pdf)
+         """) 
+    with st.expander("Navy"):
+        st.write("""
+        - [PMC Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/PMC_Book.pdf)
+        - [SCN Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/SCN_Book.pdf)
+        - [WPN Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/WPN_Book.pdf)
+        - [APN BA1-4 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/APN_BA1-4_Book.pdf)
+        - [APN BA5 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/APN_BA5_Book.pdf)
+        - [APN BA6-7 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/APN_BA6-7_Book.pdf)
+        - [OPN BA1 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/OPN_BA1_Book.pdf)
+        - [OPN BA2 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/OPN_BA2_Book.pdf)
+        - [OPN BA3 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/OPN_BA3_Book.pdf)
+        - [OPN BA4 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/OPN_BA4_Book.pdf)
+        - [OPN BA5-8 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/OPN_BA5-8_Book.pdf)
+        - [PANMC Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/PANMC_Book.pdf)
+        - [RDTEN BA1-3 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/RDTEN_BA1-3_Book.pdf)
+        - [RDTEN BA4 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/RDTEN_BA4_Book.pdf)
+        - [RDTEN BA5 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/RDTEN_BA5_Book.pdf)
+        - [RDTEN BA6 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/RDTEN_BA6_Book.pdf)
+        - [RDTEN BA7-8 Book](https://www.secnav.navy.mil/fmc/fmb/Documents/24pres/RDTEN_BA7-8_Book.pdf)
+        """)   
+    with st.expander("Army"):
+        st.write("""
+        - [Aircraft Procurement Army](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/Procurement/Aircraft%20Procurement%20Army.pdf)
+        - [Missile Procurement Army](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/Procurement/Missile%20Procurement%20Army.pdf)
+        - [Procurement of Weapons and Tracked Combat Vehicles](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/Procurement/Procurement%20of%20Weapons%20and%20Tracked%20Combat%20Vehicles.pdf)
+        - [Procurement of Ammunition Army](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/Procurement/Procurement%20of%20Ammunition%20Army.pdf)
+        - [Other Procurement - BA 1 - Tactical & Support Vehicles](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/Procurement/Other%20Procurement%20-%20BA%201%20-%20Tactical%20&%20Support%20Vehicles.pdf)
+        - [Other Procurement - BA 2 - Communications and Electronics](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/Procurement/Other%20Procurement%20-%20BA%202%20-%20Communications%20and%20Electronics.pdf)
+        - [Other Procurement - BA 3 & 4 - Other Support Equipment](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/Procurement/Other%20Procurement%20-%20BA%203%20&%204%20-%20Other%20Support%20Equipment.pdf)
+        - [RDTE Vol 1 - Budget Activity 1](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%201-Budget%20Activity%201.pdf)
+        - [RDTE Vol 1 - Budget Activity 2](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%201-Budget%20Activity%202.pdf)
+        - [RDTE Vol 1 - Budget Activity 3](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%201-Budget%20Activity%203.pdf)
+        - [RDTE Vol 2 - Budget Activity 4A](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%202-Budget%20Activity%204A.pdf)
+        - [RDTE Vol 2 - Budget Activity 4B](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%202-Budget%20Activity%204B.pdf)
+        - [RDTE Vol 2 - Budget Activity 5A](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%202-Budget%20Activity%205A.pdf)
+        - [RDTE Vol 2 - Budget Activity 5B](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%202-Budget%20Activity%205B.pdf)
+        - [RDTE Vol 2 - Budget Activity 5C](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%202-Budget%20Activity%205C.pdf)
+        - [RDTE Vol 2 - Budget Activity 5D](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%202-Budget%20Activity%205D.pdf)
+        - [RDTE Vol 3 - Budget Activity 6](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%203-Budget%20Activity%206.pdf)
+        - [RDTE Vol 3 - Budget Activity 7](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%203-Budget%20Activity%207.pdf)
+        - [RDTE Vol 3 - Budget Activity 8](https://www.asafm.army.mil/Portals/72/Documents/BudgetMaterial/2024/Base%20Budget/rdte/RDTE-Vol%203-Budget%20Activity%208.pdf)
+        """)
+
     st.write("""
-    This tool was developed by Chancee Vincent to assist in information retrieval from the following documents:
-    - [DoD RDT&E Budget Justification](https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2024/budget_justification/pdfs/03_RDT_and_E/OSD_PB2024.pdf)
-    - [DoD Procurement Budget Justification](https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2024/budget_justification/pdfs/02_Procurement/PB_2024_PDW_VOL_1.pdf)
-    - [DoD Operation and Maintenance Justification](https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2024/budget_justification/pdfs/01_Operation_and_Maintenance/O_M_VOL_1_PART_1/OM_Volume1_Part1.pdf)
-    - [Air Force Aircraft Procurement Vol I](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Aircraft%20Procurement%20Vol%20I.pdf)
-    - [Air Force Aircraft Procurement Vol II Mods](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Aircraft%20Procurement%20Vol%20II%20Mods.pdf)
-    - [Air Force Ammunition Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Ammunition%20Procurement.pdf)
-    - [Air Force Missile Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Missile%20Procurement.pdf)
-    - [Air Force Other Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Air%20Force%20Other%20Procurement.pdf)
-    - [Space Force Procurement](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Procurement/FY24%20Space%20Force%20Procurement.pdf)
-    - [Air Force RDT&E Vol I](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20I.pdf)
-    - [Air Force RDT&E Vol II](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20II.pdf)
-    - [Air Force RDT&E Vol IIIa](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20IIIa.pdf)
-    - [Air Force RDT&E Vol IIIb](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Air%20Force%20Research%20and%20Development%20Test%20and%20Evaluation%20Vol%20IIIb.pdf)
-    - [Space Force RDT&E](https://www.saffm.hq.af.mil/Portals/84/documents/FY24/Research%20and%20Development%20Test%20and%20Evaluation/FY24%20Space%20Force%20Research%20and%20Development%20Test%20and%20Evaluation.pdf)
-      
     **Example Queries**:
     - What is the budget allocation for the FAB-T Force Element Terminal for FY 2024?
     - Describe the Joint Hypersonic Transition Office's mission.
@@ -105,6 +154,7 @@ def main():
     - What is the budget activity 02: National Guard equipment listed as?
     - What is the Silent Knight Radar (SKR) Program?
     """)
+
 
     text_input = st.text_area("Type your query:", height=150)  # Using text_area for better visibility and space
     if st.button("Retrieve Information"):
